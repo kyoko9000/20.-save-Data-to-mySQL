f1e992fe39f2b0d47520ccdc2915fca66785eebc
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2024-11-23 15:57:17 +0700

    update

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 # importing libraries
-from PyQt6.QtWidgets import *
-from PyQt6 import QtCore, QtGui
-from PyQt6.QtGui import *
-from PyQt6.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import sys
 
 
